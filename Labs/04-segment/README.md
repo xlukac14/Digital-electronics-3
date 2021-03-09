5dd92135271bc4c83f3789767d555816d020ebc2
Your Name <tmarcak@gmail.com>
2021-03-09 20:07:57 +0100

    Task 3

--- Labs/04-segment/README.md
+++ Labs/04-segment/README.md
@@ -137,4 +137,53 @@ end Behavioral;
         );
 ```
 
+## 3. LED(7:4) indicators
+
+### Truth table
+
+| **Hex** | **Inputs** | **LED4** | **LED5** | **LED6** | **LED7** |
+| :-: | :-: | :-: | :-: | :-: | :-: |
+| 0 | 0000 | 1 | 0 | 0 | 0 |
+| 1 | 0001 | 0 | 0 | 1 | 1 |
+| 2 | 0010 | 0 | 0 | 0 | 1 |
+| 3 | 0011 | 0 | 0 | 1 | 0 |
+| 4 | 0100 | 0 | 0 | 0 | 1 |
+| 5 | 0101 | 0 | 0 | 1 | 0 |
+| 6 | 0110 | 0 | 0 | 0 | 0 |
+| 7 | 0111 | 0 | 0 | 1 | 0 |
+| 8 | 1000 | 0 | 0 | 0 | 1 |
+| 9 | 1001 | 0 | 0 | 1 | 0 |
+| A | 1010 | 0 | 1 | 0 | 0 |
+| b | 1011 | 0 | 1 | 1 | 0 |
+| C | 1100 | 0 | 1 | 0 | 0 |
+| d | 1101 | 0 | 1 | 1 | 0 |
+| E | 1110 | 0 | 1 | 0 | 0 |
+| F | 1111 | 0 | 1 | 1 | 0 |
+
+### VHDL code for LEDs(7:4)
+
+```vhdl
+-- Connect one common anode to 3.3V
+AN <= b"1111_0111";
+-- Display input value on LEDs
+LED(3 downto 0) <= SW;
+
+-- LED(7:4) indicators
+-- Turn LED(4) on if input value is equal to 0, ie "0000"
+LED(4)  <= '1' when (SW = "0000") else '0';
+
+-- Turn LED(5) on if input value is greater than "1001", ie 9
+LED(5)  <= '1' when (SW > "1001") else '0';
+
+-- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
+LED(6)  <= '1' when (SW = "0001" or SW = "0011" or SW = "0101" or SW = "0111" or SW = "1001" or SW = "1011" or SW = "1101" or SW = "1111")else '0';
+
+-- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
+LED(7)  <= '1' when (SW = "0001" or SW = "0010" or SW = "0100" or SW = "1000") else '0';
+```
+
+### Simulated time waveforms
+
+![](Images/waveforms_led.png)
+
 
--- Labs/04-segment/display/display/display.srcs/sources_1/new/top.vhd
+++ Labs/04-segment/display/display/display.srcs/sources_1/new/top.vhd
@@ -42,8 +42,8 @@ entity top is
            CE : out STD_LOGIC;
            CF : out STD_LOGIC;
            CG : out STD_LOGIC;
-           LED : in STD_LOGIC_VECTOR (8 - 1 downto 0);
-           AN : in STD_LOGIC_VECTOR (8 - 1 downto 0)
+           LED : out STD_LOGIC_VECTOR (8 - 1 downto 0);
+           AN : out STD_LOGIC_VECTOR (8 - 1 downto 0)
      );
 end top;
 
@@ -53,8 +53,7 @@ begin
     -- Instance (copy) of hex_7seg entity
     hex2seg : entity work.hex_7seg
         port map(
-            hex_i    => SW,
-            
+            hex_i    => SW,           
             seg_o(6) => CA,
             seg_o(5) => CB,
             seg_o(4) => CC,
@@ -64,10 +63,22 @@ begin
             seg_o(0) => CG
         );
         
-    -- Connect one common anode to 3.3V
-    AN <= b"1111_0111";
-    
-    -- Display input value on LEDs
-    LED(3 downto 0) <= SW;
-    
+-- Connect one common anode to 3.3V
+AN <= b"1111_0111";
+-- Display input value on LEDs
+LED(3 downto 0) <= SW;
+
+-- LED(7:4) indicators
+-- Turn LED(4) on if input value is equal to 0, ie "0000"
+LED(4)  <= '1' when (SW = "0000") else '0';
+
+-- Turn LED(5) on if input value is greater than "1001", ie 9
+LED(5)  <= '1' when (SW > "1001") else '0';
+
+-- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
+LED(6)  <= '1' when (SW = "0001" or SW = "0011" or SW = "0101" or SW = "0111" or SW = "1001" or SW = "1011" or SW = "1101" or SW = "1111")else '0';
+
+-- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
+LED(7)  <= '1' when (SW = "0001" or SW = "0010" or SW = "0100" or SW = "1000") else '0';
+               
 end Behavioral;
