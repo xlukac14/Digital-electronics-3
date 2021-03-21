9c6f3f6ab071aa265940aa94797eb269a93c3814
Your Name <tmarcak@gmail.com>
2021-03-21 23:16:01 +0100

    Task 2

--- Labs/06-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits.vhd
+++ Labs/06-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits.vhd
@@ -1,47 +1,14 @@
-# Lab assignment 06- Display Driver
+------------------------------------------------------------------------
+--
+-- Template for 4-digit 7-segment display driver testbench.
+-- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------------------
 
-Link to my repository: [tmarcak/Digital-electronics-1](https://github.com/tmarcak/Digital-electronics-1)
-
-## 1. Preparation tasks 
-
-### Timing diagram figure for displaying value `3.142`
-
-![](Images/timing_diagram.png)
-
-## 2. Display driver
-
-### VHDL code of the process (`p_mux`)
-
-```vhdl
-    p_mux : process(s_cnt, data0_i, data1_i, data2_i, data3_i, dp_i)
-    begin
-        case s_cnt is
-            when "11" =>
-                s_hex <= data3_i;
-                dp_o  <= dp_i(3);
-                dig_o <= "0111";
-
-            when "10" =>
-                s_hex <= data2_i;
-                dp_o  <= dp_i(2);
-                dig_o <= "1011";
-
-            when "01" =>
-                s_hex <= data1_i;
-                dp_o  <= dp_i(1);
-                dig_o <= "1101";
-
-            when others =>
-                s_hex <= data0_i;
-                dp_o  <= dp_i(0);
-                dig_o <= "1110";
-        end case;
-    end process p_mux;
-```
-
-### VHDL testbench file (`tb_driver_7seg_4digits`)
-
-```vhdl
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -134,60 +101,3 @@ begin
     end process p_stimulus;
 
 end architecture testbench;
-```
-
-### Listing of VHDL architecture of the top layer
-
-```vhdl
-architecture Behavioral of top is
--- No internal signals
-begin
---------------------------------------------------------------------
--- Instance (copy) of driver_7seg_4digits entity
-driver_seg_4 : entity work.driver_7seg_4digits
-    port map(
-        clk => CLK100MHZ,
-        
-        reset => BTNC,
-        
-        data0_i(3) => SW(3),
-        data0_i(2) => SW(2),
-        data0_i(1) => SW(1),
-        data0_i(0) => SW(0),
-        
-        data1_i(3) => SW(7),
-        data1_i(2) => SW(6),
-        data1_i(1) => SW(5),
-        data1_i(0) => SW(4),
-        
-        data2_i(3) => SW(11),
-        data2_i(2) => SW(10),
-        data2_i(1) => SW(9),
-        data2_i(0) => SW(8),
-        
-        data3_i(3) => SW(15),
-        data3_i(2) => SW(14),
-        data3_i(1) => SW(13),
-        data3_i(0) => SW(12),
-        
-        dp_i => "0111",
-        
-        dp_o => DP,
-        
-        seg_o(6) => CA,
-        seg_o(5) => CB,
-        seg_o(4) => CC,
-        seg_o(3) => CD,
-        seg_o(2) => CE,
-        seg_o(1) => CF,
-        seg_o(0) => CG,
-        
-        dig_o => AN (4-1 downto 0)
- 
-    );
-    -- Disconnect the top four digits of the 7-segment display
-    AN(7 downto 4) <= b"1111";
-
-end architecture Behavioral;
-```
-
